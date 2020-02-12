5927508d38cbdeeec9b430b9e6c6208a311eb92b
Denny Hong <denny@Dennyui-MacBookPro.local>
2020-02-12 11:56:36 +0900

    ios setting

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -3,6 +3,9 @@ import Flutter
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
+ override func application(_ app: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey : Any] = [:]) -> Bool {
+        return NaverThirdPartyLoginConnection.getSharedInstance().application(app, open: url, options: options)
+    }
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
